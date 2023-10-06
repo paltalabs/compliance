5267ec1559c821b560ff0f61cad62cefb4c4cfe0
chopan123 <chopancatrileo@gmail.com>
2023-10-06 12:39:24 -0300

    feat: user can look for an address and download a csv with all transactions

---
+++ my-next-app/pages/api/searchTxFromAddress.ts
@@ -0,0 +1,44 @@
+// pages/api/executeQuery.ts
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+import fs from 'fs';
+import fetch from 'node-fetch';
+
+// Read the .graphql file
+const graphqlQuery: string = fs.readFileSync('queries/getTransfersFromAccount.graphql', 'utf-8').toString();
+// Define the GraphQL endpoint
+const GRAPHQL_ENDPOINT: string = process.env.NEXT_APP_GRAPHQL_ENDPOINT ?? "";
+
+async function getAccountDetails(accountId: string) {
+    // Fetch using the query
+    const response = await fetch(GRAPHQL_ENDPOINT, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            query: graphqlQuery,
+            variables: { accountId: accountId }
+        })
+    });
+
+    const data = await response.json();
+    console.log("Data:", data)
+    return data;
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== 'POST') {
+        return res.status(405).end();
+    }
+
+    const { address } = req.body
+
+    try {
+        const data = await getAccountDetails(address)
+        res.status(200).json(data);
+    } catch (e) {
+        console.error("Error while fetching graphQl data: ", e)
+        res.status(500).json({ error: 'Failed to fetch account details' });
+    }
+
+
+}
--- my-next-app/pages/landing.tsx
+++ my-next-app/pages/landing.tsx
@@ -1,7 +1,70 @@
 import React, { useState } from 'react';
 
+function downloadCSV(data: any, filename = 'download.csv') {
+    // let csv = convertToCSV(data);
+    let csv = data;
+    let blob = new Blob([csv], { type: 'text/csv' });
+    let url = window.URL.createObjectURL(blob);
+
+    let a = document.createElement('a');
+    a.setAttribute('hidden', '');
+    a.setAttribute('href', url);
+    a.setAttribute('download', filename);
+    document.body.appendChild(a);
+
+    a.click();
+    document.body.removeChild(a);
+}
+
+function convertToCSV(objArray: any) {
+    const array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
+    let str = '';
+    const header = "type, blockNumber, txHash, toId, fromId, amount"
+    str = header + '\r\n'
+
+    for (let i = 0; i < array.length; i++) {
+        let line = '';
+        for (let index in array[i]) {
+            if (line !== '') line += ',';
+            line += array[i][index];
+        }
+        str += line + '\r\n';
+    }
+    return str;
+}
+
+function jsonToList(jsonData: any) {
+    const sentPaymentsNodes = jsonData.data.account.sentPayments.nodes;
+    const receivedPaymentsNodes = jsonData.data.account.receivedPayments.nodes;
+    const sentTransfersNodes = jsonData.data.account.sentTransfers.nodes;
+    const receivedTransfersNodes = jsonData.data.account.receivedTransfers.nodes;
+
+    // Map over the nodes and add the 'type' field
+    const sentPaymentsList = sentPaymentsNodes.map((payment: any) => ({
+        type: 'sentPayments',
+        ...payment
+    }));
+
+    const receivedPaymentsList = receivedPaymentsNodes.map((payment: any) => ({
+        type: 'receivedPayments',
+        ...payment
+    }));
+    const sentTransfersList = sentTransfersNodes.map((payment: any) => ({
+        type: 'sentTransfers',
+        ...payment
+    }));
+
+    const receivedTransfersList = receivedTransfersNodes.map((payment: any) => ({
+        type: 'receivedTransfers',
+        ...payment
+    }));
+    const combinedList = [...sentPaymentsList, ...receivedPaymentsList, ...sentTransfersList, ...receivedTransfersList];
+    return combinedList
+}
+
 const LandingPage: React.FC = () => {
     const [address, setAddress] = useState('');
+    const [complianceInfo, setComplianceInfo] = useState('')
 
     const handleAddressChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setAddress(e.target.value);
@@ -12,15 +75,38 @@ const LandingPage: React.FC = () => {
         // For instance, you can send the address to an API or navigate to another page.
         alert(`Getting compliance for: ${address}`);
 
+        const addressData = {
+            address: address
+        };
+
         try {
-            const response = await fetch('/api/executeQuery', { method: 'POST' });
+            const response = await fetch('/api/searchTxFromAddress', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(addressData)
+            });
+            console.log("response:", response)
+            if (!response.ok) {
+                throw new Error('Network response was not ok');  // Handle HTTP errors
+            }
             const data = await response.json();
-            console.log(data);
+            console.log("data", data)
+            // setComplianceInfo(JSON.stringify(jsonToList(data)))
+            setComplianceInfo(convertToCSV(jsonToList(data)))
         } catch (error) {
             console.error('Error executing GraphQL query:', error);
         }
     };
 
+    function DownloadButton({ data }: any) {
+        const handleDownload = () => {
+            downloadCSV(data);
+        };
+
+        return <button onClick={handleDownload}>Download CSV</button>;
+    }
     return (
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
             <h1>Welcome to Our Compliance Checker</h1>
@@ -34,6 +120,11 @@ const LandingPage: React.FC = () => {
             <button onClick={handleSubmit} style={{ padding: '10px 20px', fontSize: '16px' }}>
                 Get your compliance
             </button>
+            <a>{complianceInfo}</a>
+            {
+                (complianceInfo !== '' &&
+                    <DownloadButton data={complianceInfo} />)
+            }
         </div>
     );
 };
